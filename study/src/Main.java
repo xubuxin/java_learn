76d7d03c5d45d9ccf32e65f74fde7faac8c94048
xubuxin <xubuxin@yahoo.com>
2019-03-28 22:16:03 +0800

    something new

--- study/src/Main.java
+++ study/src/Main.java
@@ -1,9 +1,14 @@
+import java.util.ArrayList;
+import java.util.LinkedList;
+import java.util.List;
+
 /**
  * @author xubuxin
  * @date 2019/3/28 - 22:05
  */
 public class Main {
     public static void main(String[] args) {
-
+        List<Integer> integers = new ArrayList<>();
+        List<String> strings = new LinkedList<>();
     }
 }
